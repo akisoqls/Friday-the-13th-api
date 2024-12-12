841652175ac9e901a3c0730568c63f8a2d4adfee
Aki Sakata <akisoqls@gmail.com>
2024-12-13 00:00:24 +0900

    Replace with hono

--- main.ts
+++ main.ts
@@ -1,27 +1,28 @@
-import { Server } from "https://deno.land/std@0.203.0/http/server.ts";
 import { isFriday13th } from "./isFriday13th.ts";
+import { Hono } from "@hono/hono";
 
-const server = new Server({
-  port: 3000,
-  handler: (): Response => {
-    const headers = new Headers();
-    headers.set("Content-Type", "application/json");
-    headers.set("Charset", "UTF-8");
-    headers.set("Access-Control-Allow-Origin", "*");
+const app = new Hono();
 
-    const today = new Date();
+app.get("/", () => {
+  const headers = new Headers();
+  headers.set("Content-Type", "application/json");
+  headers.set("Charset", "UTF-8");
+  headers.set("Access-Control-Allow-Origin", "*");
 
-    const isItFri13th = isFriday13th(today);
-    return new Response(
-      JSON.stringify({
-        isFri13th: isItFri13th,
-      }),
-      {
-        status: 200,
-        headers,
-      },
-    );
-  },
+  const today = new Date();
+
+  const isItFri13th = isFriday13th(today);
+  return new Response(
+    JSON.stringify({
+      isFri13th: isItFri13th,
+    }),
+    {
+      status: 200,
+      headers,
+    },
+  );
 });
 
-await server.listenAndServe();
+Deno.serve({
+  port: 3000,
+}, app.fetch);
