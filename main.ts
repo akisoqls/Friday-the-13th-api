cd1f3b840cb57e952a602e891a3f3f0903b64fa2
Aki Sakata <akisoqls@gmail.com>
2024-12-13 00:06:24 +0900

    Add /the-next-Friday-the-13th endpoint

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { isFriday13th } from "./isFriday13th.ts";
 import { Hono } from "@hono/hono";
+import { getNextFriday13th } from "./nextFriday13th.ts";
 
 const app = new Hono();
 
@@ -15,6 +16,29 @@ app.get("/", () => {
   return new Response(
     JSON.stringify({
       isFri13th: isItFri13th,
+    }),
+    {
+      status: 200,
+      headers,
+    },
+  );
+});
+
+app.get("/the-next-Friday-the-13th", () => {
+  const headers = new Headers();
+  headers.set("Content-Type", "application/json");
+  headers.set("Charset", "UTF-8");
+  headers.set("Access-Control-Allow-Origin", "*");
+
+  const today = new Date();
+
+  const nextFriday = getNextFriday13th(today);
+  const nextFridayString = `${nextFriday.getFullYear()}/${
+    `${nextFriday.getMonth() + 1}`.padStart(2, "0")
+  }/${`${nextFriday.getDate()}`.padStart(2, "0")}`;
+  return new Response(
+    JSON.stringify({
+      nextFriday: nextFridayString,
     }),
     {
       status: 200,
